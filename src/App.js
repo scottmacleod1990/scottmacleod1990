a777e1df00fe00615f47f9416620766faf922174
Scott MacLeod <scott.macleod1990@gmail.com>
2022-01-31 14:09:20 -0600

    modified text in app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Test again
+          seeing if installing bootstrap helps
         </a>
       </header>
     </div>
